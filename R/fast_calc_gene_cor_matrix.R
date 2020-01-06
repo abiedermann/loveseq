40da4acc94d7b3f84ce7b699b4b444418bce2bdc
abiedermann <biederma@mit.edu>
2020-01-05 19:43:43 -0500

    DATASET.R fixes

--- R/fast_calc_gene_cor_matrix.R
+++ R/fast_calc_gene_cor_matrix.R
@@ -6,15 +6,21 @@
 #' @return list(gene.cors,this.hres): correlation data.frame with rows and columns ordered
 #'                    via hierarchical clustering. this.hres = hierarchical clustering
 #'                    object.
-calc_gene_correlation_matrix <- function(df){
+fast_calc_gene_correlation_matrix <- function(df,percent.var=0.99){
     library(fastcluster)
     library(parallelDist)
+    library(PCAtools)
     # Dropping rows with zero variance
     this.df <- df[which(apply(df,1,var) > 0),]
     # Calculating gene-to-gene correlation matrix
     gene.cors <- cor(t(this.df),t(this.df))
+    print("Running dimensionality reduction...")
+    pca.res <- pca(gene.cors,center=T,scale=T)
+    n.pcs <- min(which(cumsum(pca.res$variance)/sum(pca.res$variance)>percent.var))
     # Note: by default, parDist used the maximum number of cpu threads available
-    this.hres <- fastcluster::hclust(parDist(gene.cors))
+    print(paste0("Keeping ",as.character(n.pcs)," PCs..."))
+    print("Calculating sample distances in PCA space and clustering...")
+    this.hres <- fastcluster::hclust(parDist(gene.cors[,1:n.pcs]))
     gene.cors <- gene.cors[this.hres$order,this.hres$order]
     return(list(gene.cors,this.hres))
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,14 +1,14 @@
 library(usethis)
+library(piano)
 
 # Loading most up-to-date version of genome annotation
 annotation <- read.table("data-raw/2019Annotation.csv",header=TRUE,sep=',',quote="\"",
 		     row.names=1,stringsAsFactors=FALSE)
 
 # Pichia kegg genesets and accompanying annotations
-kegg.gmt <- read.table("data-raw/pichia_kegg_pathways.gmt",header=TRUE,sep=',',quote="\"",
-		     row.names=1,stringsAsFactors=FALSE)
-kegg.pathinfo <- read.table("data-raw/kegg_pathway_info.csv",header=TRUE,sep=',',quote="\"",
-		     row.names=1,stringsAsFactors=FALSE)
+kegg.gmt <- loadGSC("data-raw/pichia_kegg_pathways.gmt",type='gmt')
+kegg.pathinfo <- read.table("data-raw/kegg_pathway_info.csv",header=TRUE,
+			    sep='\t',quote="\"",stringsAsFactors=FALSE)
 
 
 # Note: Very important for internal=T here. This makes the data visible to
